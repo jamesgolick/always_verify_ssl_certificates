75fa6294e7aa85e941cb8b48030768ec7780e5a9
W. Andrew Loe III <andrew@andrewloe.com>
2011-09-12 14:25:03 -0700

    Move this into the library, out of the test harness.

--- lib/always_verify_ssl_certificates.rb
+++ lib/always_verify_ssl_certificates.rb
@@ -4,6 +4,7 @@ require "net/http"
 require "net/https"
 
 module AlwaysVerifySSLCertificates
+  CA_FILE = File.join(File.dirname(__FILE__), '..', 'vendor', 'cacert.pem')
 end
 
 if /^1\.9/ =~ RUBY_VERSION
--- test/helper.rb
+++ test/helper.rb
@@ -6,7 +6,5 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'always_verify_ssl_certificates'
 
-AlwaysVerifySSLCertificates::CA_FILE = File.join(File.dirname(__FILE__), '..', 'vendor', 'cacert.pem')
-
 class Test::Unit::TestCase
 end
